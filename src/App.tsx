57c2235b1933b10894c8b6a1ebe2c61bcc7fb6ff
amirhosseinsadegh <amirsadeghi7443@gmail.com>
2025-07-08 02:02:25 -0700

    structur

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandinPage from "./Commponent/Landing/LandingPage";
+import LoginPage from "./Commponent/Login/LoginPage";
+import { Toaster } from "react-hot-toast";
+import SignupPage from "./Commponent/SignupPage/SignupPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            fontFamily: "inherit",
+          },
+        }}
+      />
 
-export default App
+      <Routes>
+        <Route path="/" element={<LandinPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
